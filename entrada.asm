62259584b0a8e41dbb5e01e2cc31ddf4ca917906
Gustavo Viegas <webviegas@gmail.com>
2018-04-28 17:27:01 -0300

    Init ao trabalho, leitura do arquivo, e saida de binario

---
+++ entrada.asm
@@ -0,0 +1,5 @@
+add $s0, $s1, $s2
+or $s0, $s1, $s2
+and $t0, $s1, $zero
+sll $s3, $s2, $t0
+addi $s4 $s1 29
---
+++ main.py
@@ -0,0 +1,25 @@
+from instrucao import Instrucao
+
+print('****************************************************')
+print('*   SUPER MONTADOR MIPS - UFV Florestal            *')
+print('*  por: Bruno Marra (3029), Gustavo Viegas (3026)  *')
+print('****************************************************')
+
+print('Lendo arquivo de entrada...')
+
+with open('entrada.asm') as entrada:
+    linhas = entrada.readlines()
+
+print('Arquivo lido! Processando entrada...')
+with open('saida.txt', 'w') as saida:
+    saida.truncate()
+    for linha in linhas:
+        inst = Instrucao(linha.strip()) # Removendo whitespaces com strip
+        saida.write(inst.binario() + '\n')
+
+print('Montagem feita com sucesso! O arquivo binário está disponivel em saida.txt! \n')
+# EXEMPLOS:
+inst2 = Instrucao('addi $t0, $s0, 30')
+print(inst2.tipo())
+print(inst2.op())
+print(inst2.binario())
