a6cee8559ae1fc2aac1026bee3764c558a4dca8e
Gustavo Viegas <webviegas@gmail.com>
2018-05-17 16:03:26 -0300

    Ajuste little bug na entrada

--- input.asm
+++ input.asm
@@ -2,8 +2,8 @@ add $s0, $s1, $s2
 or $s0, $s1, $s2
 and $t0, $s1, $zero
 andi $t0, $s1, 0
-sll $s3, $s2, $t0
-srl $s0, $s1, $t0
+sll $t1, $s2, 2
+srl $s0, $s1, 3
 addi $s4 $s1 29
 sub $s0, $s1, $s2
 naoexiste $t1, 5, 2, $zero
